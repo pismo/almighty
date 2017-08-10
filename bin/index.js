e2f5d5ea8ac66f185b74ab1f2a6901fc5ba24da0
Rodrigo Pandini <rodrigopandini@gmail.com>
2017-08-10 18:06:46 -0300

    Update program version

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,7 @@ const rollbar = require('../tasks/rollbar');
 const log = console.log.bind(console);
 
 program
-    .version('0.0.2');
+    .version('0.0.3');
 
 program
   .command('release <version>')
